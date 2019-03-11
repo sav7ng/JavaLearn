70bb1a61c8a1f7de1454546782a16f7ee33700e6
853029827@qq.com <853029827@qq.com>
2019-03-11 22:50:18 +0800

    上传文件

---
+++ springboot-upload/src/main/java/github/aquan/springbootupload/controller/FileUploadController.java
@@ -0,0 +1,14 @@
+package github.aquan.springbootupload.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Aquan 2019-3-11 22:24:23
+ */
+
+@Controller
+public class FileUploadController {
+
+    private final StorageService
+
+}
---
+++ springboot-upload/src/test/java/github/aquan/springbootupload/SpringbootUploadApplicationTests.java
@@ -0,0 +1,16 @@
+package github.aquan.springbootupload;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootUploadApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
