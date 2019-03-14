4e92e8eba1aa0e60efac10509d2f3b67e1c437c1
853029827@qq.com <853029827@qq.com>
2019-03-14 18:04:02 +0800

    springboot定时任务

---
+++ springboot-scheduling-tasks/src/main/java/gitbub/aquanlerou/SpringbootSchedulingTasksApplication.java
@@ -0,0 +1,15 @@
+package gitbub.aquanlerou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class SpringbootSchedulingTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSchedulingTasksApplication.class, args);
+    }
+
+}
