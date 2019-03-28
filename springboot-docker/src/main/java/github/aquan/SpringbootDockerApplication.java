d8489039e6f1c569639e4b89740ce8826670812b
853029827@qq.com <853029827@qq.com>
2019-03-28 23:12:04 +0800

    springboot集成docker打包测试

---
+++ springboot-docker/src/main/java/github/aquan/SpringbootDockerApplication.java
@@ -0,0 +1,19 @@
+package github.aquan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootDockerApplication {
+
+	@RequestMapping("/")
+	public String home() { return "Hello Docker-Aquan"; }
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerApplication.class, args);
+	}
+
+}
