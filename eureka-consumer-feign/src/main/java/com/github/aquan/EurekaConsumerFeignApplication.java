7dd275b1e778a7e1dfce4dab62390719da3dd214
853029827@qq.com <853029827@qq.com>
2019-05-21 18:15:03 +0800

    feign

---
+++ eureka-consumer-feign/src/main/java/com/github/aquan/EurekaConsumerFeignApplication.java
@@ -0,0 +1,14 @@
+package com.github.aquan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EurekaConsumerFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConsumerFeignApplication.class, args);
+    }
+
+}
