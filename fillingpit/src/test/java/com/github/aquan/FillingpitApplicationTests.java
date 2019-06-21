7a28635b895f86eec3154b210e57db0cdfec30ab
853029827@qq.com <853029827@qq.com>
2019-06-21 17:52:15 +0800

    CalendarTest

--- fillingpit/src/test/java/com/github/aquan/FillingpitApplicationTests.java
+++ fillingpit/src/test/java/com/github/aquan/FillingpitApplicationTests.java
@@ -243,4 +243,32 @@ public class FillingpitApplicationTests {
     }
 
 
+    @Test
+    public void CalendarTest() {
+        LocalDate date = LocalDate.now();
+        int month = date.getMonthValue();
+        int today = date.getDayOfMonth();
+        date = date.minusDays(today - 1); // Set to start of month
+        DayOfWeek weekday = date.getDayOfWeek();
+        int value = weekday.getValue();
+        System.out.println("Mon Tue Wed Thu Fri Sat Sun");
+        for (int i = 1; i < value; i++)
+            System.out.print("    ");
+        while (date.getMonthValue() == month)
+        {
+            System.out.printf("%3d", date.getDayOfMonth());
+            if (date.getDayOfMonth() == today)
+                System.out.print("*");
+            else
+                System.out.print(" ");
+            date = date.plusDays(1);
+            if (date.getDayOfWeek().getValue() == 1) System.out.println();
+        }
+        if (date.getDayOfWeek().getValue() != 1) System.out.println();
+
+
+        clone()
+    }
+
+
 }
