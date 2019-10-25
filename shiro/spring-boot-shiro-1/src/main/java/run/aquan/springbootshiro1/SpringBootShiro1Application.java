475ef5e23cbaaeb7fd1090254d180135d4148e6f
853029827@qq.com <853029827@qq.com>
2019-10-25 11:46:28 +0800

    shiro.ini

---
+++ shiro/spring-boot-shiro-1/src/main/java/run/aquan/springbootshiro1/SpringBootShiro1Application.java
@@ -0,0 +1,13 @@
+package run.aquan.springbootshiro1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootShiro1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootShiro1Application.class, args);
+    }
+
+}
---
+++ shiro/spring-boot-shiro-1/src/main/java/run/aquan/springbootshiro1/shiro/MyRealm1.java
@@ -0,0 +1,40 @@
+package run.aquan.springbootshiro1.shiro;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * @Class MyRealm1
+ * @Description TODO
+ * @Author Aquan
+ * @Date 2019/10/24 16:49
+ * @Version 1.0
+ **/
+public class MyRealm1 implements Realm {
+
+    @Override
+    public String getName() {
+        return "myrealm1";
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        //仅支持UsernamePasswordToken类型的Token
+        return token instanceof UsernamePasswordToken;
+    }
+
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        String username = (String)token.getPrincipal();  //得到用户名
+        String password = new String((char[])token.getCredentials()); //得到密码
+        if(!"zhang".equals(username)) {
+            throw new UnknownAccountException(); //如果用户名错误
+        }
+        if(!"123".equals(password)) {
+            throw new IncorrectCredentialsException(); //如果密码错误
+        }
+        //如果身份认证验证成功，返回一个AuthenticationInfo实现；
+        return new SimpleAuthenticationInfo(username, password, getName());
+    }
+
+}
